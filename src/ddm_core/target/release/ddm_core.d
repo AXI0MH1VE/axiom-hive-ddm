788f52858348253e94b1d7741dce8b724f529d5b
AXI0MH1VE <235248299+AXI0MH1VE@users.noreply.github.com>
2025-12-07 20:22:40 -0500

    feat: Re-implement core DDM logic in Rust for structural safety

---
+++ src/ddm_core/target/release/ddm_core.d
@@ -0,0 +1,1 @@
+/home/ubuntu/axiom-hive-ddm/src/ddm_core/target/release/ddm_core: /home/ubuntu/axiom-hive-ddm/src/ddm_core/src/main.rs
---
+++ src/ddm_core/target/release/deps/ddm_core-fbc58cc24224c6dc.d
@@ -0,0 +1,5 @@
+/home/ubuntu/axiom-hive-ddm/src/ddm_core/target/release/deps/ddm_core-fbc58cc24224c6dc.d: src/main.rs
+
+/home/ubuntu/axiom-hive-ddm/src/ddm_core/target/release/deps/ddm_core-fbc58cc24224c6dc: src/main.rs
+
+src/main.rs:
